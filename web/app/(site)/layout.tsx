2aec306cf7707d7d8324e115f550482075fd15b4
djstep <stepan.chukalkin.2003@gmail.com>
2026-06-22 13:40:39 +0300

    Updates design

--- web/app/(site)/layout.tsx
+++ web/app/(site)/layout.tsx
@@ -1,14 +1,12 @@
 import { Header } from '@/components/layout/Header';
 import { MobileNav } from '@/components/layout/MobileNav';
 import { Footer } from '@/components/layout/Footer';
-import { SiteAmbientGlow } from '@/components/layout/SiteAmbientGlow';
 import { InquiryBridge } from '@/components/inquiry/InquiryBridge';
 import { HomeGalleryBridge } from '@/components/home/HomeGalleryBridge';
 
 export default function SiteLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <SiteAmbientGlow />
       <Header />
       {children}
       <Footer />
